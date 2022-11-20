0828ae1f325dacbbfa515278fb17290fd166ef00
piemar <pierre@thinkworks.se>
2022-11-20 14:21:37 +0100

    Updated config

--- kmip-with-hashicorp-key-vault/configuration_fle.py
+++ kmip-with-hashicorp-key-vault/configuration_fle.py
@@ -1,7 +1,7 @@
 encrypted_namespace = "DEMO-KMIP-FLE.users"
 key_vault_namespace = "DEMO-KMIP-FLE.datakeys"
-connection_uri = "mongodb+srv://<USER>:<PASSWORD>@<CLUSTER-NAME>?retryWrites=true&w=majority"
-
+connection_uri = "mongodb+srv://myadmin:X1231Xc121e@mongodb-day-demo.jf9tm8r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+shared_library_path ="/shared_lib/mongo_crypt_v1.so"
 # Configure the "kmip" provider.
 kms_providers = {
     "kmip": {
